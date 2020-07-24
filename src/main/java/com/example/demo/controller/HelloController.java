40092a3ee922ac4061f06831811171adbf58cc8a
xuehao <xuehao@wayyue.com>
2020-07-24 18:18:11 +0800

    spring

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: XueHao
+ * @Date: 2020/07/24
+ * @Copyright: Copyright (c) wayyue.com Corporation. All Rights Reserved.
+ */
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String sayHello(){
+        return "Hello SpringBoot 2.x!";
+    }
+}
